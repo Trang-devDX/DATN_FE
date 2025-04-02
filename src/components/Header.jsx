a8e7548ac0d0f4be1d78fcc5e708f8fceffaefb5
Trang-devDX <nguyenthanhtrang2733@gmail.com>
2025-04-02 12:14:48 +0700

    basic header

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,5 +1,24 @@
-export default function Header(){
+import logo from '../assets/images/logo.png'
+import { SearchOutlined, ShoppingCartOutlined, UserOutlined, ProfileOutlined, LogoutOutlined } from '@ant-design/icons';
+
+export default function Header() {
     return (
-        <div>Header</div>
-    )
+        <div className="flex justify-center">
+            <div className="w-[80%] flex flex-wrap">
+                <div className="w-[20%]">
+                    <img src={logo} alt="Logo" />
+                </div>
+                <div className="w-[45%] flex justify-center items-center">
+                    <input type="text" placeholder="Tìm kiếm sản phẩm" prefix={<SearchOutlined />} className='w-[80%] h-[50px] border-2 rounded-lg p-5'/>
+                </div>
+                <div className="w-[20%] flex justify-center items-center">
+                    <p>Hotline: 0123456789 | 0987654321</p>
+                </div>
+                <div className="w-[15%] flex items-center justify-end gap-5">
+                    <ShoppingCartOutlined className="text-2xl" />
+                    <UserOutlined className="text-2xl" />
+                </div>
+            </div>
+        </div>
+    );
 }
